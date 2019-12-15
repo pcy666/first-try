4eaf66e337acaab972a0812348807bbc3507c568
PCY <164346757@qq.com>
2019-12-15 20:07:00 +0800

    signin.html and sort out

--- Hotel/web.py
+++ Hotel/web.py
@@ -9,21 +9,31 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return render_template('index.html')
-
-@app.route('/signin', methods=['POST'])
+@app.route('/signin', methods=['GET','POST'])
 def signin():
-    # 需要从request对象读取表单内容：
-    uname = request.form['username']
-    pwd = request.form['password']
-    way = request.values.get('options')
+    if request.method == 'POST':
+        # 需要从request对象读取表单内容：
+        status = request.values.get('status')
+        if status == '2':
+            uname = request.form['guest_name']
+            uid = request.form['guest_id']
+            unum = request.form['guest_num']
+            value = (uname,uid,unum)
+            db.dbinsert('clients',value)
+            return render_template('signin 2.html')
+        else:
+            uname = request.form['username']
+            pwd = request.form['password']
+            way = request.values.get('options')
 
-    if pwd == db.dbsearch('staff','id',uname)[0][1]:  #判断是否和数据库中的密码一致
+            if pwd == db.dbsearch('staff','id',uname)[0][1]:  #判断是否和数据库中的密码一致
+                return render_template('signin.html')
+            else:
+                return render_template('index.html')
+    elif request.method == 'GET':
         return render_template('signin.html')
-    else:
-        return render_template('index.html')
+    
+
 
 @app.route('/ChooseRoom',methods=['POST'])
 def ChooseRoom():
