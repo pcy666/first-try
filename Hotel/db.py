540be7b2b8fdb6c3434c86126334662e6757f3bf
PCY <164346757@qq.com>
2019-12-20 10:07:46 +0800

    function complete  little

--- Hotel/db.py
+++ Hotel/db.py
@@ -6,14 +6,31 @@ def dbsearch(table,index,value):
         conn = MySQLdb.connect('localhost','root','root','宾馆住房',charset = 'utf8')
     except:
         print('can\'t connect with MySQL')
+        return 0
     cursor = conn.cursor()
-
     try:
-        cursor.execute("select * from {} where {} = {}".format(table,index,value))
+        cursor.execute("select * from {} where {} = \'{}\'".format(table,index,value))
         result = cursor.fetchall()
         return result
     except:
         conn.rollback()
+        return 0
+    conn.close()
+
+def dbsearchAll(table,target):
+    try:
+        conn = MySQLdb.connect('localhost','root','root','宾馆住房',charset = 'utf8')
+    except:
+        print('can\'t connect with MySQL')
+        return 0
+    cursor = conn.cursor()
+    try:
+        cursor.execute("select {} from {}".format(target,table))
+        result = cursor.fetchall()
+        return result
+    except:
+        conn.rollback()
+        return 0
     conn.close()
 
 def dbinsert(table,value):
@@ -21,6 +38,7 @@ def dbinsert(table,value):
         conn = MySQLdb.connect('localhost','root','root','宾馆住房',charset = 'utf8')
     except:
         print('can\'t connect with MySQL')
+        return 0
     cursor = conn.cursor()
     try:
         cursor.execute('insert into {} values {}'.format(table,value))
@@ -28,6 +46,38 @@ def dbinsert(table,value):
         return 'insert success'
     except:
         conn.rollback()
+        return 0
+
+def dbDelete(table,target,value):
+    try:
+        conn = MySQLdb.connect('localhost','root','root','宾馆住房',charset = 'utf8')
+    except:
+        print('can\'t connect with MySQL')
+        return 0
+    cursor = conn.cursor()
+    try:
+        cursor.execute("delete from {} where {} = \'{}\'".format(table,target,value))
+        cursor.execute()
+        return 'delete success'
+    except:
+        conn.rollback()
+        return 0
+
+
+def dbchangeOneNum(table,attr,attr_value,attr_change,change_value):
+    try:
+        conn = MySQLdb.connect('localhost','root','root','宾馆住房',charset = 'utf8')
+    except:
+        print('can\'t connect with MySQL')
+        return 0
+    cursor = conn.cursor()
+    try:
+        cursor.execute("update {} set {}=\'{}\' where {}=\'{}\'".format(table,attr_change,change_value,attr,attr_value))
+        cursor.execute()
+        return True
+    except:
+        conn.rollback()
+        return 0
 
 if __name__ == "__main__":
     data = "('601','6000','0','总统套房')"
--- Hotel/web.py
+++ Hotel/web.py
@@ -26,64 +26,94 @@ def home():
 #普通员工管理网页
 @app.route('/signin/<id>', methods=['GET','POST'])
 def signin(id):
+    #存储可用房间的列表
+    single = []
+    double = []
+    luxury = []
+    suite = []
+    high = []
+    president = []
+    for s in db.dbsearch('room','type','单人间'):  #访问数据库，查找所有房间
+        if s[2] == 0:  #判断是否有人
+            single.append(s)
+    for s in db.dbsearch('room','type','双人间'):
+        if s[2] == 0:
+            double.append(s)
+    for s in db.dbsearch('room','type','豪华房'):
+        if s[2] == 0:
+            luxury.append(s)
+    for s in db.dbsearch('room','type','套间'):
+        if s[2] == 0:
+            suite.append(s)
+    for s in db.dbsearch('room','type','高级套间'):
+        if s[2] == 0:
+            high.append(s)
+    for s in db.dbsearch('room','type','总统套房'):
+        if s[2] == 0:
+            president.append(s)
+
     if request.method == 'POST':
-        # 需要从request对象读取表单内容：
-        status = request.values.get('status')  #用于判断用户当前处在的状态
-        if status == '2':
-            uname = request.form['guest_name']
-            uid = request.form['guest_id']
-            unum = request.form['guest_num']
-            value = (uname,uid,unum)
+        type = request.values.get('type')
+        if type == '2':
+            # 需要从request对象读取表单内容：
+            list = request.get_data()
+            name = request.values.get('name')
+            id = request.values.get('id')
+            num = request.values.get('num')
+            value = (name,id,num)
             db.dbinsert('clients',value)
 
-            #存储可用房间的列表
-            single = []
-            double = []
-            luxury = []
-            suite = []
-            high = []
-            president = []
-            for s in db.dbsearch('room','type','\'单人间\''):  #访问数据库，查找所有房间
-                if s[2] == 0:  #判断是否有人
-                    single.append(s[0])
-            for s in db.dbsearch('room','type','\'双人间\''):
-                if s[2] == 0:
-                    double.append(s[0])
-            for s in db.dbsearch('room','type','\'豪华房\''):
-                if s[2] == 0:
-                    luxury.append(s[0])
-            for s in db.dbsearch('room','type','\'套间\''):
-                if s[2] == 0:
-                    suite.append(s[0])
-            for s in db.dbsearch('room','type','\'高级套间\''):
-                if s[2] == 0:
-                    high.append(s[0])
-            for s in db.dbsearch('room','type','\'总统套房\''):
-                if s[2] == 0:
-                    president.append(s[0])
-            return render_template('signin 2.html',single = single, double = double,luxury = luxury,suite = suite,high = high,president = president)
+            return render_template('signin.html',single = single, double = double,luxury = luxury,suite = suite,high = high,president = president)
+        elif type == '4':  #续住
+            room_id = request.values.get('roomid')
+            days = request.values.get('days')
+
+        elif type == '5':  #房间查询
+            room_id = request.values.get('roomid')
+        elif type == '6':  #退房
+            room_id = request.values.get('roomid')
         else:
-            return render_template('signin.html')
+            return render_template('signin.html',single = single, double = double,luxury = luxury,suite = suite,high = high,president = president)
     elif request.method == 'GET':
-        return render_template('signin.html')
+        return render_template('signin(1).html',single = single, double = double,luxury = luxury,suite = suite,high = high,president = president)
     
 #管理员网页
 @app.route('/admin/<id>',methods=['GET','POST'])
 def admin(id):
     if request.method == 'GET':
-        return render_template('admin.html')
+        single = db.dbsearch('room','type','单人间')[0][1]
+        double = db.dbsearch('room','type','双人间')[0][1]
+        luxury = db.dbsearch('room','type','豪华房')[0][1]
+        suite = db.dbsearch('room','type','套间')[0][1]
+        high = db.dbsearch('room','type','高级套间')[0][1]
+        president = db.dbsearch('room','type','总统套房')[0][1]
+        staff = db.dbsearchAll('staff','*')
+        return render_template('admin.html',single = single, double = double,luxury = luxury,suite = suite,high = high,president = president,staff = staff)
     elif request.method == 'POST':
         num = request.values.get('num')
-        type = request.values.get('type')
+        type = request.values.get('type')  #功能位，决定使用的功能
         newkey =request.values.get('newkey')
         uname =request.values.get('uname')
         psw =request.values.get('psw')
         price =request.values.get('price')
         #房间种类 num
-        # if type == '1': #修改房间价格
-        # elif type == '2': #修改密码
-        # elif type == '3': #删除管理员账户
-        return render_template('index.html')
+        if type == '1': #修改房间价格
+            if num == '1':
+                db.dbchangeOneNum('room','type','单人间','price',int(price))
+            elif num == '2':
+                db.dbchangeOneNum('room','type','双人间','price',int(price))
+            elif num == '3':
+                db.dbchangeOneNum('room','type','豪华房','price',int(price))
+            elif num == '4':
+                db.dbchangeOneNum('room','type','套间','price',int(price))
+            elif num == '5':
+                db.dbchangeOneNum('room','type','高级套间','price',int(price))
+            elif num == '6':
+                db.dbchangeOneNum('room','type','总统套房','price',int(price))
+        elif type == '2': #修改密码
+            db.dbchangeOneNum('staff','id',num,'pwd',newkey)
+        elif type == '3': #删除管理员账户
+            db.dbDelete('staff','id',uname)
 
 @app.route('/ChooseRoom',methods=['POST'])
 def ChooseRoom():
